095aaa306c041240e8e90c0c74adc376a499db65
dimasyoga42 <dimasyoga9090@gmail.com>
2024-06-08 15:24:48 +0700

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind()]
 });
