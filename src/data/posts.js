f6254a41025b9f7d10d5fefc6f5f925c53babe37
dimasyoga42 <dimasyoga9090@gmail.com>
2024-06-08 19:32:12 +0700

    add card

---
+++ src/data/posts.js
@@ -0,0 +1,18 @@
+export const posts = [
+  {
+    id: 1,
+    title: 'Cara membuat website',
+    des: 'oke kali ini gue akan memberi tutorial membuat website dari nol',
+    time: '10 juni 2024'
+  }, 
+  {
+    id: 2,
+    title: 'Cara uploud project ke github',
+    des: 'Kali ini gue akan memberikan tutorial cara uploud project yg kita punya di github',
+    time: '10 juni 2024'
+  }
+  ];
+  
+  export function getAllpost() {
+    return posts;
+  }
