c5b4190f6e621ce0544ca3fe2556dc4abb3f63b4
dimasyoga42 <dimasyoga9090@gmail.com>
2024-06-12 07:51:31 +0700

    final commit

--- src/data/posts.js
+++ src/data/posts.js
@@ -10,6 +10,12 @@ export const posts = [
     title: 'Cara uploud project ke github',
     des: 'Kali ini gue akan memberikan tutorial cara uploud project yg kita punya di github',
     time: '10 juni 2024'
+  },
+    {
+    id: 3,
+    title: 'Tutorial makan',
+    des: 'Kali ini gue akan memberikan tutorial cara makan yang baik dan benar',
+    time: '10 juni 2024'
   }
   ];
   
